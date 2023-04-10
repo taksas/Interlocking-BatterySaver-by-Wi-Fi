344a074653dbd5e7195e9ae4fa74085c31cdc45b
Taksas <takuminakayama4869@gmail.com>
2023-04-10 12:20:45 +0900

    プロジェクト ファイルを追加します。

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Minimal_BatterySaver_Enabler__with_Wi_Fi_
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
