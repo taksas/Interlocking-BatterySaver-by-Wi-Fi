f54fbc41c7482a342ff67d027d1cc58e545e32de
Taksas <takuminakayama4869@gmail.com>
2023-04-10 13:08:38 +0900

    [Test] Battery Saver ON OFF Buttons

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +24,38 @@ namespace Minimal_BatterySaver_Enabler__with_Wi_Fi_
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Process オブジェクトを生成
+            System.Diagnostics.Process p = new System.Diagnostics.Process();
+            //ComSpec(cmd.exe)のパスを取得して、FileNameプロパティに指定
+            p.StartInfo.FileName = System.Environment.GetEnvironmentVariable("ComSpec");
+            //ウィンドウを表示しないようにする
+            p.StartInfo.CreateNoWindow = true;
+
+            string cmd = " powercfg /setdcvalueindex SCHEME_CURRENT SUB_ENERGYSAVER ESBATTTHRESHOLD 100 && powercfg /setactive scheme_current";
+            p.StartInfo.Arguments = @"/c " + cmd;
+            //起動
+            p.Start();
+            
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            // Process オブジェクトを生成
+            System.Diagnostics.Process p = new System.Diagnostics.Process();
+            //ComSpec(cmd.exe)のパスを取得して、FileNameプロパティに指定
+            p.StartInfo.FileName = System.Environment.GetEnvironmentVariable("ComSpec");
+            //ウィンドウを表示しないようにする
+            p.StartInfo.CreateNoWindow = true;
+
+            string cmd = " powercfg /setdcvalueindex SCHEME_CURRENT SUB_ENERGYSAVER ESBATTTHRESHOLD 0 && powercfg /setactive scheme_current";
+            p.StartInfo.Arguments = @"/c " + cmd;
+            //起動
+            p.Start();
         }
     }
 }
